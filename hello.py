91a200999032260e6d2144690c4a977165e62e9a
SurajxTech <surajchauhanofficials@gmail.com>
2024-09-29 17:44:25 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+# Introduction Script
+
+def main():
+    print("Welcome to the Hello World Project!")
+    print("This project is created by Suraj Chauhan.")
+    print("Enjoy exploring the code and learning more about GitHub projects.")
+
+if __name__ == "__main__":
+    main()
